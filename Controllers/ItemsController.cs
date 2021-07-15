4f75b8d5fa71d46e909f7b547c0c2d03c364303d
Nikola Vrhovac <Nikola.Vrhovac@gvcgroup.com>
2021-07-15 18:18:43 +0200

    remove ignored files

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Catalog.Entities;
 using Catalog.Repositories;
@@ -22,6 +23,14 @@ namespace Catalog.Controllers
     {
       var items = repository.GetItems();
       return items;
+    }
+
+    // GET /items/id
+    [HttpGet("{id}")]
+    public Item GetItem(Guid id)
+    {
+      var item = repository.GetItem(id);
+      return item;
     }
   }
 }
